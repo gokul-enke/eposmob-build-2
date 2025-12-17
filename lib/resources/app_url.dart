9a9e4450092cc536ba4a0e6d4b7d0d4d2f921f60
Gokul Hans V <gokulhansv@gmail.com>
2025-12-17 18:15:29 +0530

    eeezee build

--- lib/resources/app_url.dart
+++ lib/resources/app_url.dart
@@ -6,11 +6,11 @@ class APPUrl {
   //  static String baseURL = "https://stagingepos.enke.ae";
   // static String baseURL = "https://tenant.hypersouq.in";
   // static String baseURL = "https://eposdemo.yougoit.in";
-  // static String baseURL = "https://eeezeeerp.cloudposai.com";
+  static String baseURL = "https://eeezeeerp.cloudposai.com";
   // static String baseURL = "https://eposdemo.yougoit.in";
   // static String baseURL = "https://eeezee.enke.ae";
   // static String baseURL = "https://actdemo.yougoit.in";
-  static String baseURL = "https://epos.hypersouq.in";
+//   static String baseURL = "https://epos.hypersouq.in";
   // static String baseURL = "https://epos.yougoit.in";
   // static String baseURL = "https://open-poems-sneeze.loca.lt";
   // static String baseURL = "http://localhost:8000";
