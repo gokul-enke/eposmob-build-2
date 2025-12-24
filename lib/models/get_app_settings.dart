971f5cb0c3a807c590056b1b7313a3b71fe34e3e
Gokul Hans V <gokulhansv@gmail.com>
2025-12-24 11:43:50 +0530

    default tax and mrp false

--- lib/models/get_app_settings.dart
+++ lib/models/get_app_settings.dart
@@ -95,7 +95,7 @@ class AppSettings {
       zatcaPhase1Enabled: settingsMap['ZATCA_PHASE_1']?['status'] ?? false,
       zatcaPhase2Enabled: settingsMap['ZATCA_PHASE_2']?['status'] ?? false,
       showTaxPos: settingsMap['SHOW_TAX_POS']?['status'] ?? false,
-      showMrpPos: settingsMap['SHOW_MRP_POS']?['status'] ?? true,
+      showMrpPos: settingsMap['SHOW_MRP_POS']?['status'] ?? false,
     );
   }
 
