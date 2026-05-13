be4a7d438e6ed49e3911fceb64ab0d5cbaef1624
Thanseer2024 <thanseer2024@gmail.com>
2026-05-13 10:56:48 +0530

    dynamic sidemenu

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.enke.cloudpos"
+    namespace = "com.enke.cloudposai"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.enke.cloudpos"
+        applicationId = "com.enke.cloudposai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -39,14 +39,14 @@ android {
         keystoreProperties.load(keystorePropertiesFile.inputStream())
     }
 
-//    signingConfigs {
-//        create("release") {
-//            keyAlias = keystoreProperties["keyAlias"] as String?
-//            keyPassword = keystoreProperties["keyPassword"] as String?
-//            storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
-//            storePassword = keystoreProperties["storePassword"] as String?
-//        }
-//    }
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
 
     buildTypes {
         release {
